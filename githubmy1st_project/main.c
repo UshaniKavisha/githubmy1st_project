c3e6eb15995451f143565a0cf7fbf69b2f99b837
UshaniKavisha <as20240494@sci.sjp.ac.lk>
2025-07-22 09:25:59 +0530

    Update main.c

--- githubmy1st_project/main.c
+++ githubmy1st_project/main.c
@@ -3,7 +3,16 @@
 
 int main()
 {
+    int i;
+
     printf("Hello world!\n");
-    printf("Hello Sri Jayawardhana Pura..\n");
+    printf("Hello university of srijayawardhanapura\n\n\n\n");
+
+
+    for(i=0;i<5;i++)
+    {
+        printf("%d",i);
+    }
     return 0;
+
 }
